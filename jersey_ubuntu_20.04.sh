3e95af49cd8df6022c69e762ca4e036e5239fa0a
santosh653 <70637961+santosh653@users.noreply.github.com>
2021-06-28 10:57:24 +0530

    Update jersey_ubuntu_20.04.sh
    build script

--- jersey_ubuntu_20.04.sh
+++ jersey_ubuntu_20.04.sh
@@ -1,13 +1,13 @@
 # ----------------------------------------------------------------------------
 #
-# Package	    : jersey
-# Version	    : 0.1.0 
-# Source repo	: https://github.com/eclipse-ee4j/jersey
-# Tested on	    : ubuntu_20.04
-# Script License: Apache License, Version 2 or later
-# Maintainer	: Santosh Kulkarni <santoshkulkarni70@gmail.com> / Priya Seth<sethp@us.ibm.com>
+# Package           : jersey
+# Version           : 3.0.1
+# Source repo       : https://github.com/eclipse-ee4j/jersey
+# Tested on         : ubuntu_20.04
+# Script License    : Apache License, Version 2 or later
+# Maintainer        : Santosh Kulkarni <santoshkulkarni70@gmail.com> / Priya Seth<sethp@us.ibm.com>
 #
-# Disclaimer: This script has been tested in non-root mode on given
+# Disclaimer        : This script has been tested in non-root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
@@ -16,24 +16,37 @@
 # ----------------------------------------------------------------------------
 #!/bin/bash
 
+
+export REPO=https://github.com/eclipse-ee4j/jersey
+
+if [ -z "$1" ]; then
+  export VERSION="3.0.1"
+else
+  export VERSION="$1"
+fi
+
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk wget git -y
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-ppc64el/
-
-wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-tar xzvf apache-maven-3.5.2-bin.tar.gz
-export PATH=$PATH:`pwd`/apache-maven-3.5.2/bin
 sudo apt install  -y maven
 mvn -version
-git clone https://github.com/eclipse-ee4j/jersey
+
+if [ -d "jersey" ] ; then
+  rm -rf jersey
+fi
+
+git clone ${REPO}
+
 ## Build and test jersey
 cd jersey
-mvn clean install
+git checkout ${VERSION}
+ret=$?
 
-## Note ##
-# Running build Without Tests
-# mvn clean install -DskipTests=true
-# Then Running Tests saperately 
-# mvn test
-# Running build and test in same command 
-# mvn clean install
+if [ $ret -eq 0 ] ; then
+  echo "$Version found to checkout "
+else
+  echo "$Version not found "
+  exit
+fi
+
+mvn clean install
